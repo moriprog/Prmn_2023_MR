177f442ab1df7f70d3a4cac6d724c7006b854dee
smkck377 <ujimattya0213@outlook.jp>
2023-09-05 17:31:10 +0900

    9/05活動

---
+++ Assets/StraightScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float speed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(0, 1, 0);
+    }
+
+    // Update is called once per frame
+    /*void FixedUpdate()
+    {
+        if (rb.velocity.magnitude < 10)
+        {
+            
+            float currentSpeed = speed - rb.velocity.magnitude;
+            
+            rb.AddForce(new Vector3(0, currentSpeed, 0));
+            
+        }
+    }*/
+}
