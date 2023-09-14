1b00faa0d99ec9fc74856a87667dd8bc6ceeb714
[moriprog] <[b2212410@photon.chitose.ac.jp>
2023-09-14 23:26:54 +0900

    9/14作業分

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -26,6 +26,28 @@ public class PlayerController : MonoBehaviour
 
     void Straight()
     {
+        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+        //rb.constraints = RigidbodyConstraints.FreezeRotationX.None; //Rotaitionのxをオン
+        Vector3 now = rb.position;            // 座標を取得
+        now += new Vector3(0.0f, 0.0f, 0.05f);  // 前に少しずつ移動するように加算
+        rb.position = now; // 値を設定
+    }
 
+    void Right()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+        //rb.constraints = RigidbodyConstraints.FreezeRotationX.None; //Rotaitionのxをオン
+        Vector3 now = rb.position;            // 座標を取得
+        now += new Vector3(0.0f, 0.005f,0.0f);  // 前に少しずつ移動するように加算
+        rb.position = now; // 値を設定
+    }
+
+    void Left()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+        //rb.constraints = RigidbodyConstraints.FreezeRotationX.None; //Rotaitionのxをオン
+        Vector3 now = rb.position;            // 座標を取得
+        now += new Vector3(0.0f, -0.005f, 0.0f);  // 前に少しずつ移動するように加算
+        rb.position = now; // 値を設定
     }
 }
