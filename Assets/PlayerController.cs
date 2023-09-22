da262e2dad365b842283cd5975530b23b59c33c3
[moriprog] <[b2212410@photon.chitose.ac.jp>
2023-09-22 16:55:50 +0900

    9/21作業分

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,22 +6,54 @@ public class PlayerController : MonoBehaviour
 {
     float speed;
     Vector2 startPos;
+    private Rigidbody rb;
+    private bool isMoving = true;
+    private float initialY; // 初期のy座標
+    private float distanceTraveled = 0f; // 移動した距離
+
+    // 移動速度
+    public float moveSpeed = 0.145f;
+    // 移動する距離
+    public float moveDistance = 0.145f;
+
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
+        rb = GetComponent<Rigidbody>();
+        initialY = transform.position.y; // 初期のy座標を保存
+        Debug.Log("現在のy座標: " + initialY);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+        //Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
         //rb.constraints = RigidbodyConstraints.FreezeRotationX.None; //Rotaitionのxをオン
-        Vector3 now = rb.position;            // 座標を取得
-        now += new Vector3(0.0f, 0.005f, 0.0f);  // 前に少しずつ移動するように加算
-        rb.position = now; // 値を設定
+        //Vector3 now = rb.position;            // 座標を取得
+        //now += new Vector3(0.0f, 0.005f, 0.0f);  // 前に少しずつ移動するように加算
+        //rb.position = now; // 値を設定
+        //Debug.Log(now.y);
         //this.speed = 0.0002f;
         //transform.Translate(0, 0, -this.speed);
+        if (isMoving && distanceTraveled < moveDistance)
+        {
+            // y軸方向に移動
+            Vector3 movement = new Vector3(0, moveSpeed, 0);
+            rb.MovePosition(transform.position + movement);
+
+            // 移動した距離を更新
+            distanceTraveled += moveSpeed * Time.deltaTime;
+
+            // 移動距離が目標距離以上になったら移動を停止
+            if (distanceTraveled >= moveDistance)
+            {
+                rb.constraints =RigidbodyConstraints.FreezeRotationY;
+                isMoving = false;
+                // Rigidbodyの速度をゼロに設定して停止させる
+                rb.velocity = Vector3.zero;
+            }
+        }
     }
 
     void Straight()
